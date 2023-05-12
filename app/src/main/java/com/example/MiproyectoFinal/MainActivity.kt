9e8f820f042b460a5584eda989c4862a505f296b
JuniorDLR <juniordelarocha15@hotmail.com>
2023-05-11 22:50:18 -0600

    JR

--- app/src/main/java/com/example/MiproyectoFinal/MainActivity.kt
+++ app/src/main/java/com/example/MiproyectoFinal/MainActivity.kt
@@ -8,7 +8,7 @@ import com.example.MiproyectoFinal.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    val junior: String = ""
+    val junior: String = "JUNIOR"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
